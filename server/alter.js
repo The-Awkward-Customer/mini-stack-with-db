97e014b375e361d4c6b7eb08bdf12f13f7204633
PETER ABBOTT <abbotsayshi@gmail.com>
2023-12-21 16:33:56 +0000

    added alter code

---
+++ server/alter.js
@@ -0,0 +1,30 @@
+import Database from "better-sqlite3";
+
+const db = new Database("database.db");
+
+try {
+  db.exec(`
+CREATE TABLE new_messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    message TEXT
+)
+`);
+
+  db.exec(`
+INSERT INTO new_messages (username, message)
+    SELECT username, message
+    FROM messages;
+`);
+
+  db.exec(`DROP TABLE messages;`);
+
+  db.exec(`ALTER TABLE new_messages RENAME TO messages;`);
+
+  console.log("ID column added successfully, and data migrated.");
+} catch (error) {
+  console.error("Error during the process:", error.message);
+} finally {
+  // Close the database connection
+  db.close();
+}
--- server/seed.js
+++ server/seed.js
@@ -3,7 +3,7 @@ import Database from "better-sqlite3";
 const db = new Database("database.db");
 
 db.exec(`CREATE TABLE IF NOT EXISTS messages(
-    id INT AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT,
     message TEXT
 )`);
